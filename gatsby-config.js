2b613c27903980fcd9394fa5a2d1b52028aacb0d
Takaya-Yamazato <yamazato@m.ieice.org>
2020-12-15 16:23:45 +0900

    https://github.com/netlify-templates/gatsby-starter-netlify-cms/issues/646

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+//    'gatsby-plugin-sass',
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
@@ -14,6 +14,12 @@ module.exports = {
         path: `${__dirname}/static/img`,
         name: 'uploads',
       },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        indentedSyntax: true
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
