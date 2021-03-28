29cda506cdd7610239bcca3d8289d9b2862cf4a8
Takaya-Yamazato <yamazato@m.ieice.org>
2021-03-28 23:04:17 +0900

    ちょっと修正

--- src/templates/recommendations-page.js
+++ src/templates/recommendations-page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 import Layout from '../components/Layout'
-// import Content, { HTMLContent } from '../components/Content'
+
 
 // export default function RecommendationsPage() {
 //   const data = useStaticQuery(graphql`
