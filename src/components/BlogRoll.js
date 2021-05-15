abf45eea76d430e9bd30779d25286dee2a6a7522
Takaya-Yamazato <yamazato@m.ieice.org>
2021-05-15 22:43:18 +0900

    warningを潰す

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -65,36 +65,74 @@ BlogRoll.propTypes = {
 }
 
 
-export default () => (
-  <StaticQuery
-    query={graphql`query BlogRollQuery {
-  allMarkdownRemark(
-    sort: {order: DESC, fields: [frontmatter___date]}
-    filter: {frontmatter: {templateKey: {eq: "blog-post"}}}
-  ) {
-    edges {
-      node {
-        excerpt(pruneLength: 400)
-        id
-        fields {
-          slug
-        }
-        frontmatter {
-          title
-          templateKey
-          date(formatString: "MMMM DD, YYYY")
-          featuredpost
-          featuredimage {
-            childImageSharp {
-              gatsbyImageData(width: 240, quality: 100, layout: CONSTRAINED)
+export default function BlogRollQuery() {
+  return (
+    <StaticQuery
+      query={graphql`
+      query  {
+        allMarkdownRemark(
+          sort: {order: DESC, fields: [frontmatter___date]}
+          filter: {frontmatter: {templateKey: {eq: "blog-post"}}}
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 400)
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                templateKey
+                date(formatString: "MMMM DD, YYYY")
+                featuredpost
+                featuredimage {
+                  childImageSharp {
+                    gatsbyImageData(width: 240, quality: 100, layout: CONSTRAINED)
+                  }
+                }
+              }
             }
           }
         }
       }
-    }
-  }
+      `}
+       render={(data, count) => <BlogRoll data={data} count={count} />}
+    />
+  )
 }
-`}
-    render={(data, count) => <BlogRoll data={data} count={count} />}
-  />
-)
+
+// export default () => (
+//   <StaticQuery
+//     query={graphql`query BlogRollQuery {
+//   allMarkdownRemark(
+//     sort: {order: DESC, fields: [frontmatter___date]}
+//     filter: {frontmatter: {templateKey: {eq: "blog-post"}}}
+//   ) {
+//     edges {
+//       node {
+//         excerpt(pruneLength: 400)
+//         id
+//         fields {
+//           slug
+//         }
+//         frontmatter {
+//           title
+//           templateKey
+//           date(formatString: "MMMM DD, YYYY")
+//           featuredpost
+//           featuredimage {
+//             childImageSharp {
+//               gatsbyImageData(width: 240, quality: 100, layout: CONSTRAINED)
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+// `}
+//     render={(data, count) => <BlogRoll data={data} count={count} />}
+//   />
+// )
+
