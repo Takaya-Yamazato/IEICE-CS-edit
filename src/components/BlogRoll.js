c7ba1b1d793d7ccde10c65a69502854a71c3bab2
Takaya-Yamazato <yamazato@m.ieice.org>
2021-05-15 22:44:46 +0900

    コメントアウトを削除

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -102,37 +102,3 @@ export default function BlogRollQuery() {
   )
 }
 
-// export default () => (
-//   <StaticQuery
-//     query={graphql`query BlogRollQuery {
-//   allMarkdownRemark(
-//     sort: {order: DESC, fields: [frontmatter___date]}
-//     filter: {frontmatter: {templateKey: {eq: "blog-post"}}}
-//   ) {
-//     edges {
-//       node {
-//         excerpt(pruneLength: 400)
-//         id
-//         fields {
-//           slug
-//         }
-//         frontmatter {
-//           title
-//           templateKey
-//           date(formatString: "MMMM DD, YYYY")
-//           featuredpost
-//           featuredimage {
-//             childImageSharp {
-//               gatsbyImageData(width: 240, quality: 100, layout: CONSTRAINED)
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
-// `}
-//     render={(data, count) => <BlogRoll data={data} count={count} />}
-//   />
-// )
-
