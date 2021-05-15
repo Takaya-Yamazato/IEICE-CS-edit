2e382922571a1a1e425a0110cb337f8bd0523b15
Takaya-Yamazato <yamazato@m.ieice.org>
2021-05-15 15:35:21 +0900

    backgound image の url を変更

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -13,7 +13,7 @@ const IndexPageTemplate = ({ data }) => (
     <div
       className="full-width-image margin-top-0"
       style={{
-        backgroundImage: `url('../img/home-jumbotron.jpg')`,
+        backgroundImage: `url('img/home-jumbotron.jpg')`,
         // backgroundImage: {data.markdownRemark.image},
         backgroundPosition: `top left`,
         backgroundAttachment: `fixed`
