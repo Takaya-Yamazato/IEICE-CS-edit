faab8f21f6d3420daae6ebb088c38fe4654581ff
Takaya YAMAZATO <yamazato@ilas.nagoya-u.ac.jp>
2024-09-13 18:03:37 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,7 +100,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-netlify-cms`,
+      // resolve: `gatsby-plugin-netlify-cms`,
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
@@ -114,7 +114,7 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    `gatsby-plugin-netlify`, // make sure to keep it last in the array
+    // `gatsby-plugin-netlify`, // make sure to keep it last in the array
     `gatsby-plugin-meta-redirect`, // make sure to put last in the array
   ],
 }
