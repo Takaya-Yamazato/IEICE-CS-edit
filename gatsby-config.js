884c733cfffbd145a0d4f9e3aaec0cbee80c390f
Takaya YAMAZATO <yamazato@ilas.nagoya-u.ac.jp>
2024-09-13 17:54:24 +0900

    npm へ変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,12 +99,12 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-netlify-cms`,
+    //   options: {
+    //     modulePath: `${__dirname}/src/cms/cms.js`,
+    //   },
+    // },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     {
@@ -114,7 +114,7 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    `gatsby-plugin-netlify`, // make sure to keep it last in the array
+    // `gatsby-plugin-netlify`, // make sure to keep it last in the array
     `gatsby-plugin-meta-redirect`, // make sure to put last in the array
   ],
 }
